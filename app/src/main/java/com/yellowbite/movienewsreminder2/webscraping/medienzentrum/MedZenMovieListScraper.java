451b26ed74669144f12c9bf99bf644a62c36a16b
JanNB97 <j.bingemann@gmx.de>
2018-07-28 09:26:54 +0200

    ListScraper only provides essential movies

--- app/src/main/java/com/yellowbite/movienewsreminder2/webscraping/medienzentrum/MedZenMovieListScraper.java
+++ app/src/main/java/com/yellowbite/movienewsreminder2/webscraping/medienzentrum/MedZenMovieListScraper.java
@@ -42,40 +42,6 @@ public class MedZenMovieListScraper
         return new Movie(mediaBarcode, link);
     }
 
-    public Movie getMovie(int index)
-    {
-        //essentials
-        Movie movie = getEssentialMovie(index);
-        if(movie == null)
-        {
-            return null;
-        }
-
-        //status infos
-        Status status = getStatus(index);
-        movie.setStatus(status);
-        int vorbestellungen = getVorbestellungen(index);
-        movie.setVorbestellungen(vorbestellungen);
-        Date entliehenBis = getEntliehenBis(index);
-        movie.setEntliehenBis(entliehenBis);
-
-        //standort infos
-        String standort = getStandort(index);
-        movie.setStandort(standort);
-        String interessenkreis = getInteressenkreis(index);
-        movie.setInteressenkreis(interessenkreis);
-        String signatur = getSignatur(index);
-        movie.setSignatur(signatur);
-
-        //useful infos
-        String titel = getTitel(index);
-        movie.setTitel(titel);
-        String kurzbeschreibung = getKurzbeschreibung(index);
-        movie.setKurzbeschreibung(kurzbeschreibung);
-
-        return movie;
-    }
-
     private Element getListEntry(int index)
     {
         if(index < 0 || index >= listEntries.size())
@@ -101,67 +67,5 @@ public class MedZenMovieListScraper
     public String getLink(int index)
     {
         return WebscrapingHelper.getLink(getListEntry(index), "a[href]");
-    }
-
-    // --- get status informations ---
-
-    public Status getStatus(int index)
-    {
-        return WebscrapingHelper.getStatus(getListEntry(index), "[title='entliehen']");
-    }
-
-    public int getVorbestellungen(int index)
-    {
-        String vorbestellungen = WebscrapingHelper.getText(getListEntry(index), "span.reservationCount");
-
-        if(vorbestellungen == null)
-        {
-            return 0;
-        }
-
-        String[] split = vorbestellungen.split(" ");
-
-        try
-        {
-            return Integer.parseInt(split[0]);
-        } catch (NumberFormatException e)
-        {
-            Logger.getGlobal().severe("\"Vorbestellung\" don't have the right format");
-            return -1;
-        }
-    }
-
-    public Date getEntliehenBis(int index)
-    {
-        return WebscrapingHelper.getDate(getListEntry(index), "span.borrowUntil");
-    }
-
-    // --- get standort informations ---
-
-    public String getStandort(int index)
-    {
-        return WebscrapingHelper.getText(getListEntry(index), "span.location");
-    }
-
-    public String getInteressenkreis(int index)
-    {
-        return WebscrapingHelper.getText(getListEntry(index), "span.topics");
-    }
-
-    public String getSignatur(int index)
-    {
-        return WebscrapingHelper.getText(getListEntry(index), "span.systematik");
-    }
-
-    // --- get other useful informations ---
-
-    public String getTitel(int index)
-    {
-        return WebscrapingHelper.getText(getListEntry(index),"[title='Titel']");
-    }
-
-    public String getKurzbeschreibung(int index)
-    {
-        return WebscrapingHelper.getText(getListEntry(index), "[title='Kurzbeschreibung']");
     }
 }
