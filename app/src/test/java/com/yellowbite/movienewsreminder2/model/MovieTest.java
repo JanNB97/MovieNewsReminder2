6640db947d7a9791b403c797742ed06fa55957d6
JanNB97 <j.bingemann@gmx.de>
2018-07-28 17:59:24 +0200

    Added test for compareTo

---
+++ app/src/test/java/com/yellowbite/movienewsreminder2/model/MovieTest.java
@@ -0,0 +1,12 @@
+package com.yellowbite.movienewsreminder2.model;
+
+import org.junit.Test;
+
+public class MovieTest
+{
+    @Test
+    public void testCompareTo()
+    {
+        // TODO
+    }
+}
