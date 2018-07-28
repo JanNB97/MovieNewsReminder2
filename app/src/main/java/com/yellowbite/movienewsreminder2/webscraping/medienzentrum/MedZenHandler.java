b955e11f972f3bc5efceff8687e194dee17b788d
JanNB97 <j.bingemann@gmx.de>
2018-07-28 14:55:49 +0200

    more code cleanup

--- app/src/main/java/com/yellowbite/movienewsreminder2/webscraping/medienzentrum/MedZenHandler.java
+++ app/src/main/java/com/yellowbite/movienewsreminder2/webscraping/medienzentrum/MedZenHandler.java
@@ -8,7 +8,8 @@ public class MedZenHandler extends WebscrapingHandler
 {
     public static final String MED_ZEN_NEW_MOVIES_URL = "https://opac.winbiap.net/mzhr/acquisitions.aspx?data=Y21kPTUmYW1wO3NDPWNfMD0xJSVtXzA9MSUlZl8wPTYzJSVvXzA9NiUldl8wPTI1LjA4LjIwMTYgMDA6MDA6MDArK2NfMT0xJSVtXzE9MSUlZl8xPTQyJSVvXzE9MSUldl8xPTQ2Uy1EVkQgKFNwaWVsZmlsbSkrK2NfMj0xJSVtXzI9MSUlZl8yPTQ4JSVvXzI9MSUldl8yPU1lZGllbnplbnRydW0gSGVyc2ZlbGQtUm90ZW5idXJnJmFtcDtTb3J0PVp1Z2FuZ3NkYXR1bSAoQmlibGlvdGhlayk=-B/ZW6RDg8Xg=";
 
-    public MedZenHandler() {
+    public MedZenHandler()
+    {
         super("Medienzentrum");
     }
 
