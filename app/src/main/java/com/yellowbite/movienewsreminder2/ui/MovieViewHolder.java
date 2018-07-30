c56b8546549971f5821b5719efe700f0526c206c
JanNB97 <j.bingemann@gmx.de>
2018-07-30 20:41:09 +0200

    Status is shown more nicely

--- app/src/main/java/com/yellowbite/movienewsreminder2/ui/MovieViewHolder.java
+++ app/src/main/java/com/yellowbite/movienewsreminder2/ui/MovieViewHolder.java
@@ -1,11 +1,13 @@
 package com.yellowbite.movienewsreminder2.ui;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.yellowbite.movienewsreminder2.R;
 import com.yellowbite.movienewsreminder2.model.Movie;
+import com.yellowbite.movienewsreminder2.util.DateHelper;
 
 public class MovieViewHolder extends RecyclerView.ViewHolder
 {
@@ -23,6 +25,31 @@ public class MovieViewHolder extends RecyclerView.ViewHolder
     public void showMovie(Movie movie)
     {
         titelTextView.setText(movie.getTitel());
-        statusTextView.setText(movie.getStatus().getValue());
+
+        if(movie.getStatus() == null)
+        {
+            return;
+        }
+
+        switch (movie.getStatus())
+        {
+            case VERFUEGBAR:
+                statusTextView.setText(movie.getStatus().getValue());
+                break;
+            case ENTLIEHEN:
+                if(movie.getVorbestellungen() > 0)
+                {
+                    statusTextView.setText(movie.getVorbestellungen() + " Vor.");
+                }
+                else
+                {
+                    statusTextView.setText(DateHelper.toString(movie.getEntliehenBis()));
+                }
+
+                break;
+            case VORBESTELLT:
+                statusTextView.setText(movie.getVorbestellungen() + " Vor.");
+                break;
+        }
     }
 }
