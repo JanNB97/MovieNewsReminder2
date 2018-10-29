042f302bfd935cb85bb286980aa8ea4511c2aa13
JanNB97 <j.bingemann@gmx.de>
2018-10-29 21:28:54 +0100

    listen for navigationdrawer actions

--- app/src/main/java/com/yellowbite/movienewsreminder2/ui/activites/NavigationDrawerActivity.java
+++ app/src/main/java/com/yellowbite/movienewsreminder2/ui/activites/NavigationDrawerActivity.java
@@ -1,12 +1,13 @@
 package com.yellowbite.movienewsreminder2.ui.activites;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import com.yellowbite.movienewsreminder2.R;
-import com.yellowbite.movienewsreminder2.ui.activites.ToolbarActivity;
 
 public abstract class NavigationDrawerActivity extends ToolbarActivity
 {
@@ -16,10 +17,28 @@ public abstract class NavigationDrawerActivity extends ToolbarActivity
     protected void modifyToolbar()
     {
         this.drawerLayout = this.findViewById(R.id.drawer_layout);
+        this.initNavigationView(this.findViewById(R.id.nav_view));
 
         ActionBar actionBar = this.getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    private void initNavigationView(NavigationView navigationView)
+    {
+        navigationView.getMenu().getItem(0).setChecked(true);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            item.setChecked(true);
+            drawerLayout.closeDrawers();
+            handleOnNavItemClicked(item);
+            return true;
+        });
+    }
+
+    private void handleOnNavItemClicked(@NonNull MenuItem item)
+    {
+        // TODO
     }
 
     @Override
