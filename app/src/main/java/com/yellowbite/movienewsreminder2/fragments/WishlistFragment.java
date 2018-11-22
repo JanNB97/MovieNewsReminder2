e51db323eab1b103517f0422ba3e40d8d94c43f5
JanNB97 <j.bingemann@gmx.de>
2018-11-22 15:38:22 +0100

    add elements to wishlist

--- app/src/main/java/com/yellowbite/movienewsreminder2/fragments/WishlistFragment.java
+++ app/src/main/java/com/yellowbite/movienewsreminder2/fragments/WishlistFragment.java
@@ -2,13 +2,21 @@ package com.yellowbite.movienewsreminder2.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.yellowbite.movienewsreminder2.R;
+import com.yellowbite.movienewsreminder2.data.Movie;
+import com.yellowbite.movienewsreminder2.datastructures.fromfile.unsorted.WishedMoviesList;
+import com.yellowbite.movienewsreminder2.fragments.ui.recyclerview.MovieRecyclerView;
 
 public class WishlistFragment extends ToolbarFragment
 {
     public static final int FRAGMENT_ID = 3;
+
+    private MovieRecyclerView wishedMovieRecyclerView;
+    private FloatingActionButton addWishedMovieButton;
 
     public WishlistFragment()
     {
@@ -24,11 +32,40 @@ public class WishlistFragment extends ToolbarFragment
 
     private void findViewsById()
     {
-        // TODO
+        this.addWishedMovieButton = super.getView().findViewById(R.id.addWishedMovieFloatingButton);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        this.initialize();
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private void initialize()
+    {
+        this.initRecyclerView();
+        this.initFloatingButton();
+    }
+
+    private void initRecyclerView()
+    {
+        this.wishedMovieRecyclerView = new MovieRecyclerView(this.getActivity(),
+                R.id.wishedMoviesRecyclerView, WishedMoviesList.getInstance(this.getContext()),
+                true, R.layout.simple_movie_list_row);
+        this.wishedMovieRecyclerView.showItems();
+    }
+
+    private void initFloatingButton()
+    {
+        this.addWishedMovieButton.setOnClickListener(this::handleOnAddMovieClicked);
     }
 
     public void handleOnAddMovieClicked(View view)
     {
         // TODO
+        Movie movie = new Movie(32423, "");
+        movie.setTitel("Test");
+        this.wishedMovieRecyclerView.addItem(movie);
     }
 }
