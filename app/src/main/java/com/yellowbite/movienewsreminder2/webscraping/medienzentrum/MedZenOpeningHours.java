eca1601b03f4afdd7750de93f243801fbcb497a0
JanNB97 <j.bingemann@gmx.de>
2018-11-26 20:45:49 +0100

    Added MedZenOpeningHours class

---
+++ app/src/main/java/com/yellowbite/movienewsreminder2/webscraping/medienzentrum/MedZenOpeningHours.java
@@ -0,0 +1,6 @@
+package com.yellowbite.movienewsreminder2.webscraping.medienzentrum;
+
+public class MedZenOpeningHours
+{
+    public static final String OPENING_HOURS_URL = "https://opac.winbiap.net/mzhr/index.aspx?data=cGFnZUlkPTE2-K5D47jXuuW4=";
+}
